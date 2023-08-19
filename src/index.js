2ce4bd96952bcc3393ad3eece51a783ae86d9f05
Sahno007 <turnskin90@gmail.com>
2023-08-19 12:23:35 +0300

    upg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-165
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import Notiflix from "notiflix";
+
+const breedSelect = document.querySelector(".breed-select");
+const catInfo = document.querySelector(".cat-info");
+const loader = document.querySelector(".loader");
+const error = document.querySelector(".error");
+
+function renderBreedOptions(breeds) {
+  breedSelect.innerHTML = breeds.map(breed => {
+    return `<option value="${breed.id}">${breed.name}</option>`;
+  }).join("");
+}
+
+function renderCatInfo(cat) {
+  catInfo.innerHTML = `
+    <img src="${cat.url}" alt="${cat.breeds[0].name}">
+    <h2>${cat.breeds[0].name}</h2>
+    <p>Description: ${cat.breeds[0].description}</p>
+    <p>Temperament: ${cat.breeds[0].temperament}</p>
+  `;
+}
+
+function showLoader() {
+  loader.classList.remove("hidden");
+}
+
+function hideLoader() {
+  loader.classList.add("hidden");
+}
+
+function showError(message) {
+  error.textContent = message;
+  error.classList.remove("hidden");
+}
+
+function hideError() {
+  error.classList.add("hidden");
+}
+
+breedSelect.addEventListener("change", event => {
+  const breedId = event.target.value;
+  
+  showLoader();
+  hideError();
+    catInfo.innerHTML = "";
+    
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      renderCatInfo(cat);
+      hideLoader();
+    })
+    .catch(error => {
+      showError("Error fetching cat information.");
+      hideLoader();
+    });
+});
+
+showLoader();
+hideError();
+
+fetchBreeds()
+  .then(breeds => {
+    renderBreedOptions(breeds);
+    hideLoader();
+  })
+  .catch(error => {
+    showError("Error fetching breed list.");
+    hideLoader();
+  });
